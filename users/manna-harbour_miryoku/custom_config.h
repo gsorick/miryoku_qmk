66f7a6343195ccd5781ac55f90727b754cfd919b
gsorick <93161679+gsorick@users.noreply.github.com>
2024-11-26 12:51:56 +0100

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -53,5 +53,3 @@ TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA
 KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_SCRL,           KC_F4,             KC_F5,             KC_F6,             KC_F11,            \
 U_NA,              KC_ALGR,           TD(U_TD_U_MEDIA),  TD(U_TD_U_FUN),    U_NA,              KC_PAUS,           KC_F1,             KC_F2,             KC_F3,             KC_F10,            \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_APP,            U_NP,              U_NP
-
-U_NP,	U_NP,	U_NA,	U_NA,	U_NA,	KC_ENT,	KC_BSPC,	KC_APP,	U_NP,	U_NP	
